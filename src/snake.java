8518751e50ddfd5a2a0753efa57875a83864ae94
RUCHIT680 <ruchitrai282@gmail.com>
2020-09-27 20:17:37 +0530

    added UC4

--- src/snake.java
+++ src/snake.java
@@ -1,22 +1,31 @@
-public class uc3{
+public class uc4_2{
         public static void main(String[] args){
-        double die=Math.floor(Math.random()*10)%6;
+
         int pos=0;
-        int no=(int)die+1;
+        int counter=0;
+        while (pos<=100){
         int mov=(int)(Math.floor(Math.random()*10)%3);
-        System.out.println("Number on die is " +no);
-        pos=pos+no;
-        System.out.println("New position is :" +pos);
+        int die=(int)(Math.floor(Math.random()*10)%6);
         switch(mov){
         case 1:
-        {System.out.println("Player doesn't move");
-	break;}
+
+        	pos=pos+0;
+			break;
         case 2: 
-        {System.out.println("Player moves forward");
-	break;}
+
+        	pos=pos+die+1;
+        	break;
         default:
-        {System.out.println("Player moves backward"); 
-	break;}
+
+        	pos=pos-die-1;
+         	break;
                }
-        }
-}
+        System.out.println("Number on die is " +(die+1));
+        System.out.println("New position is "+pos);
+if (pos<0)
+{pos=0;
+counter++;}
+else {pos=pos;
+counter++ ;};}
+System.out.println(" "+counter);        
+}}
